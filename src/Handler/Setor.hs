0333c1cd8f6e2b343774a1a45fb4a0c24de01e24
Adriano <adrianocostaads@gmail.com>
2018-12-08 18:47:29 +0000

    Config form e Formulario e Cadastro de Setor

---
+++ src/Handler/Setor.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
+module Handler.Setor where
+
+import Import
+import Text.Lucius
+
+formSetor :: Form Setor
+formSetor = renderBootstrap $ Setor 
+    <$> areq textField "Descricao do Sigla: " Nothing
+    <*> areq textField "Descricao do Setor: " Nothing
+
+getSetorR :: Handler Html
+getSetorR = do 
+    (widgetForm, enctype) <- generateFormPost formSetor
+    defaultLayout $ do
+        addStylesheet $ StaticR css_menu2_css
+        toWidget $(luciusFile "templates/homepage.lucius")
+        $(whamletFile "templates/hamlet/setor.hamlet")
+    
+postSetorR :: Handler Html 
+postSetorR = do 
+    ((res,_),_) <- runFormPost formSetor
+    case res of 
+        FormSuccess setor -> do
+            _ <- runDB $ insert setor
+            redirect SetorR
+        x -> do 
+            defaultLayout [whamlet|
+                <h1>
+                    #{show x}
+            |]
